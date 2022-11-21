b4c38730da7531ad9e4eddea324779e285d3af7d
pprzemek56 <przemkopis@wp.pl>
2022-11-21 01:52:52 +0100

    add child class which will represent object of child in app

---
+++ entites/Child.py
@@ -0,0 +1,20 @@
+class Child:
+    def __init__(self, name, birth_date):
+        self.name = name
+        self.birth_date = birth_date
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def birth_date(self):
+        return self._birth_date
+    
+    @birth_date.setter
+    def birth_date(self, birth_date):
+        self._birth_date = birth_date
