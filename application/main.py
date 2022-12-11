7efae95efe9d09c5668a74c3e18827ca114ce109
pprzemek56 <przemkopis@wp.pl>
2022-12-12 00:46:59 +0100

    change color

--- application/main.py
+++ application/main.py
@@ -5,6 +5,7 @@ from core.screen_manager import WindowManager
 
 class VaccinationCalendarApp(MDApp):
     def build(self):
+        self.theme_cls.primary_palette = "Green"
         return WindowManager()
 
 
