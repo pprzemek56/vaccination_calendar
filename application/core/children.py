623b06e8a5aeebf74fd9aa7bb83583b939db6dd0
pprzemek56 <przemkopis@wp.pl>
2022-12-11 23:45:09 +0100

    add file of representation of every option in main menu .py and .kv

---
+++ application/core/children.py
@@ -0,0 +1,8 @@
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+
+Builder.load_file("layouts/children.kv")
+
+
+class Children(Screen):
+    pass
---
+++ application/core/vaccination.py
@@ -0,0 +1,8 @@
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+
+Builder.load_file("layouts/vaccination.kv")
+
+
+class Vaccination(Screen):
+    pass
