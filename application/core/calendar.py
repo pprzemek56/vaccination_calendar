a9e498861dbf9dc971c8f7d89aee3c6a52d4f109
pprzemek56 <przemkopis@wp.pl>
2022-12-14 01:29:51 +0100

    fix bug with fitimage and add GridLayout with two labels of month and years and 4 buttons for changing the actuall date on the screen

--- application/core/calendar.py
+++ application/core/calendar.py
@@ -1,8 +1,25 @@
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
+from datetime import date
+
+
+month_name = ["styczeń", "luty", "marzec", "kwiecień", "maj", "czerwiec",
+              "lipiec", "sierpień", "wrzesień", "październik", "listopad", "grudzień"]
 
 Builder.load_file("layouts/calendar.kv")
 
 
 class Calendar(Screen):
-    pass
+
+    calendar_date = date.today()
+
+    def on_enter(self, *args):
+        self.ids.year_label.text = f"{self.calendar_date.year}"
+        self.ids.month_label.text = f"{month_name[self.calendar_date.month - 1].upper()}"
+
+    def month_to_left(self):
+        pass
+
+    def month_to_right(self):
+        pass
+
