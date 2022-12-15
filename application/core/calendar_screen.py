6018a0395873ef784f0fd2e757843de75df9fe09
pprzemek56 <przemkopis@wp.pl>
2022-12-15 21:26:22 +0100

    fix get_calendar function and hotfix screen_manager.kv

--- application/core/calendar_screen.py
+++ application/core/calendar_screen.py
@@ -4,7 +4,6 @@ from kivy.uix.screenmanager import Screen
 from datetime import date
 from calendar import monthcalendar
 
-
 month_name = ["styczeń", "luty", "marzec", "kwiecień", "maj", "czerwiec",
               "lipiec", "sierpień", "wrzesień", "październik", "listopad", "grudzień"]
 
@@ -12,16 +11,16 @@ Builder.load_file("layouts/calendar.kv")
 
 
 class Calendar(Screen):
-
     calendar_date = date.today()
 
     def on_enter(self, *args):
         self.ids.year_label.text = f"{self.calendar_date.year}"
         self.ids.month_label.text = f"{month_name[self.calendar_date.month - 1].upper()}"
-
-        for i in range(7):
+        print(f"year = {self.calendar_date.year}\n month = {self.calendar_date.month}")
+        for i in range(6):
             for j in range(7):
-                self.ids.calendar_layout.add_widget(Label(text=f"{i}:{j}", size_hint=(.142, .142), color=(0, 0, 0, 1)))
+                calendar = self.get_calendar(self.calendar_date.year, self.calendar_date.month)
+                self.ids.calendar_layout.add_widget(Label(text=f"{calendar[i][j]}", size_hint=(.142, .142), color=(0, 0, 0, 1)))
 
     def month_to_left(self):
         pass
@@ -29,13 +28,14 @@ class Calendar(Screen):
     def month_to_right(self):
         pass
 
-    def get_calendar(self, month, year):
-        previous_month = monthcalendar(year - 1 if month == 1 else year, 12 if month == 1 else month)[4]
-        next_month = monthcalendar(year + 1 if month == 12 else year, 1 if month == 12 else month)
+    def get_calendar(self, year, month):
+        previous_month = monthcalendar(year - 1 if month == 1 else year, 12 if month == 1 else month - 1)[4]
+        next_month = monthcalendar(year + 1 if month == 12 else year, 1 if month == 12 else month + 1)[0]
         calendar = [["pon", "wto", "śrd", "czw", "pią", "sob", "nie"]]
-
+        print(f"previous month = {previous_month}")
+        print(f"next month = {next_month}")
         for i in range(5):
-            calendar.append(monthcalendar(2022, 1)[i])
+            calendar.append(monthcalendar(year, month)[i])
             if i == 0:
                 for j in range(7):
                     if calendar[i + 1][j] == 0:
