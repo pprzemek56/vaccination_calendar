9bf5c5ead679238bc203490c6c46972e74e28283
pprzemek56 <przemkopis@wp.pl>
2022-12-20 22:07:17 +0100

    add mthods for changing months and years and add new icon 27w.png

--- application/core/calendar_screen.py
+++ application/core/calendar_screen.py
@@ -1,11 +1,10 @@
 from kivy.lang import Builder
-from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen
 from datetime import date
 from calendar import monthcalendar, weekheader
 
 from kivymd.uix.button import MDIconButton
-from kivymd.uix.label import MDIcon
+
 
 month_name = ["styczeń", "luty", "marzec", "kwiecień", "maj", "czerwiec",
               "lipiec", "sierpień", "wrzesień", "październik", "listopad", "grudzień"]
@@ -21,25 +20,50 @@ class Calendar(Screen):
         self.ids.year_label.text = f"{self.calendar_date.year}"
         self.ids.month_label.text = f"{month_name[self.calendar_date.month - 1].upper()}"
 
+        self.generate_calendar()
+
+    def generate_calendar(self):
+        self.ids.calendar_layout.clear_widgets()
         for weekday in weekheader(3).split(" "):
             label = MDIconButton(icon=f"images/icons/{weekday}.png", size_hint=(1, 1), disabled=True)
             self.ids.calendar_layout.add_widget(label)
-
         for i in range(5):
             for j in range(7):
                 calendar = self.get_calendar(self.calendar_date.year, self.calendar_date.month)
                 btn = MDIconButton(icon=f"images/icons/numeric-{calendar[i + 1][j]}.png", size_hint=(1, 1))
                 self.ids.calendar_layout.add_widget(btn)
 
+    def change_month(self, side):
+        active_month = self.calendar_date.month
+        if side == "left":
+            if active_month == 1:
+                self.calendar_date = self.calendar_date.replace(month=self.calendar_date.month + 11)
+            else:
+                self.calendar_date = self.calendar_date.replace(month=self.calendar_date.month - 1)
+        else:
+            if active_month == 12:
+                self.calendar_date = self.calendar_date.replace(month=self.calendar_date.month - 11)
+            else:
+                self.calendar_date = self.calendar_date.replace(month=self.calendar_date.month + 1)
 
-    def month_to_left(self):
-        pass
+        self.ids.month_label.text = f"{month_name[self.calendar_date.month - 1].upper()}"
+        self.generate_calendar()
 
-    def month_to_right(self):
-        pass
+    def change_year(self, side):
+
+        if side == "left":
+            self.calendar_date = self.calendar_date.replace(year=self.calendar_date.year - 1)
+        else:
+            self.calendar_date = self.calendar_date.replace(year=self.calendar_date.year + 1)
+
+        self.ids.year_label.text = f"{self.calendar_date.year}"
+        self.generate_calendar()
+
 
     def get_calendar(self, year, month):
-        previous_month = monthcalendar(year - 1 if month == 1 else year, 12 if month == 1 else month - 1)[4]
+        previous_month = monthcalendar(year - 1 if month == 1 else year,
+                                       12 if month == 1 else month - 1)[len(monthcalendar(year - 1 if month == 1 else year,
+                                                                                          12 if month == 1 else month - 1)) - 1]
         next_month = monthcalendar(year + 1 if month == 12 else year, 1 if month == 12 else month + 1)[0]
         calendar = [["pon", "wto", "śrd", "czw", "pią", "sob", "nie"]]
 
