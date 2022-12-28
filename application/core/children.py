29dc2cc1278046a0f065aad1e7c719529babb557
pprzemek56 <przemkopis@wp.pl>
2022-12-28 18:07:10 +0100

    fix errors with duplicated items in children_lits

--- application/core/children.py
+++ application/core/children.py
@@ -10,14 +10,16 @@ Builder.load_file("layouts/children.kv")
 
 
 class Children(Screen):
-    first_enter = True
-    children_list = vaccination_calendar.get_children()
-    list_elements = []
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.children_list = None
+        self.list_elements = []
 
     def on_enter(self, *args):
-        if self.first_enter:
-            for child in self.children_list:
-                item = Builder.load_string(f'''OneLineAvatarIconListItem:
+        self.children_list = vaccination_calendar.get_children()
+
+        for child in self.children_list:
+            item = Builder.load_string(f'''OneLineAvatarIconListItem:
     text: "{child['name']}"
     on_release:
         app.root.current = "child"
@@ -25,10 +27,15 @@ class Children(Screen):
         
     IconLeftWidget:
         icon: "images/icons/numeric-{child['id']}.png"''')
-                self.list_elements.append(item)
+            self.list_elements.append(item)
 
-            for element in self.list_elements:
-                self.ids.children_list.add_widget(element)
+        for element in self.list_elements:
+            self.ids.children_list.add_widget(element)
 
-            self.first_enter = False
+    def on_leave(self, *args):
+
+        for i in range(len(self.list_elements)):
+            self.ids.children_list.remove_widget(self.list_elements[i])
+
+        self.list_elements.clear()
 
