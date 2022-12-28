d3f245df619a1c271799547e9e5b7f68d41fd9c9
pprzemek56 <przemkopis@wp.pl>
2022-12-28 23:58:35 +0100

    create first representation of name and date filed on child screen

--- application/core/child.py
+++ application/core/child.py
@@ -5,4 +5,12 @@ Builder.load_file("layouts/child.kv")
 
 
 class Child(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.current_id = None
+
+    def on_enter(self, *args):
+        print(self.ids)
+
+    def show_date_picker(self):
+        pass
