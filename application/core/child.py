a51c87e11a03a9db2f760e53beea8ea116d64288
pprzemek56 <przemkopis@wp.pl>
2022-12-29 19:02:14 +0100

    create edit buttons for name and date in child window

--- application/core/child.py
+++ application/core/child.py
@@ -13,6 +13,7 @@ class Child(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.current_id = None
+        self.child = None
 
     @property
     def current_id(self):
@@ -22,9 +23,38 @@ class Child(Screen):
     def current_id(self, current_id):
         self._current_id = current_id
 
+    @property
+    def child(self):
+        return self._child
+
+    @child.setter
+    def child(self, child):
+        self._child = child
+
     def on_enter(self, *args):
-        pass
-        # self.ids.edit_name.ids.text_field.text =
+        self.child = vaccination_calendar.get_child(self.current_id)
+        self.ids.edit_name.ids.text_field.text = self.child["name"]
+        self.ids.edit_name.ids.text_field.icon_left = "account"
+        self.ids.edit_name.ids.edit_btn.on_release = self.edit_name_btn
+        self.ids.edit_date.ids.text_field.text = self.child["birth_date"]
+        self.ids.edit_date.ids.text_field.icon_left = "calendar"
+        self.ids.edit_date.ids.edit_btn.on_release = self.edit_date_btn
 
     def show_date_picker(self):
         pass
+
+    def edit_name_btn(self):
+        if self.ids.edit_name.ids.edit_btn.icon == "pencil-lock":
+            self.ids.edit_name.ids.edit_btn.icon = "pencil"
+            self.ids.edit_name.ids.text_field.disabled = False
+            self.ids.edit_name.ids.text_field.focus = True
+        else:
+            self.ids.edit_name.ids.edit_btn.icon = "pencil-lock"
+            self.ids.edit_name.ids.text_field.disabled = True
+            self.ids.edit_name.ids.text_field.text = self.child["name"]
+
+    def edit_date_btn(self):
+        if self.ids.edit_date.ids.edit_btn.icon == "pencil-lock":
+            self.ids.edit_date.ids.edit_btn.icon = "pencil"
+        else:
+            self.ids.edit_date.ids.edit_btn.icon = "pencil-lock"
