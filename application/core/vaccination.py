8db63e8cd3129aa3330392dbd41a692ea045b685
pprzemek56 <przemkopis@wp.pl>
2022-12-30 21:01:44 +0100

    create function for fetch vaccination by name

--- application/core/vaccination.py
+++ application/core/vaccination.py
@@ -2,6 +2,8 @@ import sys
 
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.dialog import MDDialog
 from kivymd.uix.list import OneLineAvatarIconListItem, IconLeftWidget
 
 Builder.load_file("layouts/vaccination.kv")
@@ -15,6 +17,7 @@ class Vaccination(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.vaccination_names = vaccination_calendar.get_vaccination()
+        self.dialog = None
 
     def on_enter(self, *args):
         if not self.ids.vaccination_list.children:
@@ -24,5 +27,17 @@ class Vaccination(Screen):
                 self.ids.vaccination_list.add_widget(item)
 
     def open_vaccination(self, instance):
-        pass
+        vaccination = vaccination_calendar.get_vaccination_by_name(instance.text)
 
+        self.dialog = MDDialog(
+                title=f"Szczepionka przeciw {vaccination['name']}",
+                text=f"{vaccination['information']}\nIlość dawek: {vaccination['dose']}\nSzczepienie {'obowiązkowe' if vaccination['mandatory'] else 'nieobowiązkowe'}",
+                buttons=[
+                    MDFlatButton(
+                        text="OKEY",
+                        on_release=self.close_dialog)])
+        self.dialog.open()
+
+
+    def close_dialog(self, obj):
+        self.dialog.dismiss()
