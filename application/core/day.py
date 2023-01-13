69d4f1e0b9feaca5aa204e35a80e03dd0997b348
pprzemek56 <przemkopis@wp.pl>
2023-01-13 18:45:16 +0100

    create carousel

--- application/core/day.py
+++ application/core/day.py
@@ -1,7 +1,19 @@
+import sys
+from datetime import date
+
 from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen
+from kivymd.uix.label import MDLabel
+
+sys.path.append('database')
+import vaccination_calendar
 
 Builder.load_file("layouts/day.kv")
+
+
+class CarouselItem(BoxLayout):
+    pass
 
 
 class Day(Screen):
@@ -16,3 +28,11 @@ class Day(Screen):
     @current_id.setter
     def current_id(self, current_id):
         self._current_id = current_id
+
+    def on_enter(self, *args):
+        vaccinations = vaccination_calendar.get_notifications(date.fromisoformat(self.current_id))
+
+        for vaccination in vaccinations:
+            box_layout = BoxLayout()
+            box_layout.add_widget(MDLabel(text=f"{vaccination['name']}"))
+            self.ids.carousel.add_widget(box_layout)
